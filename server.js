ae41b9aaba8fa17f41bf442686ff512ced186a9e
Artyom Ulanchik <artyom.u@outlook.com>
2022-04-12 22:35:06 +0100

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.listen(() => {
-  console.log("Server is running");
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+
+app.get("/users", function (req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData,
+  });
+});
+
+app.listen(8000, function () {
+  console.log("server is listening");
 });
